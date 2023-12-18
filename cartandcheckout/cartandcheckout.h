d2428ddb04b56b8ccd6dfec97649023d2f873dd5
Al Dockett <al.dockett@gmail.com>
2023-12-18 16:27:47 -0500

    Initial Commit

---
+++ cartandcheckout/cartandcheckout.h
@@ -0,0 +1,18 @@
+//
+//  cartandcheckout.h
+//  cartandcheckout
+//
+//  Created by Al Dockett on 12/18/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cartandcheckout.
+FOUNDATION_EXPORT double cartandcheckoutVersionNumber;
+
+//! Project version string for cartandcheckout.
+FOUNDATION_EXPORT const unsigned char cartandcheckoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cartandcheckout/PublicHeader.h>
+
+
